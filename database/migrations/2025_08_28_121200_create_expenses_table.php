9690a3998f555ca73d328dcb4d7fc137d10b57de
unknown <supradi765@gmail.com>
2025-08-31 10:33:05 +0800

    swipe database mysql to posgresql

--- database/migrations/2025_08_28_121200_create_expenses_table.php
+++ database/migrations/2025_08_28_121200_create_expenses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->integer('amount');
             $table->text('notes');
             $table->date('date');
